87fa320694f46bc59b90ab15fd3ef5520d91c133
Mitul Dudhat <mituldudhat42@gmail.com>
2022-05-20 11:49:52 +0530

    commiting try

--- 1java/src/Mitul/S_Binary_Que_1.java
+++ 1java/src/Mitul/S_Binary_Que_1.java
@@ -59,4 +59,43 @@ public class S_Binary_Que_1 {
         }
         return ans;
     }
+
+    public int splitArray(int[] nums, int m) {
+        //https://leetcode.com/problems/split-array-largest-sum/
+        int start = 0;
+        int end = 0;
+
+        for (int j : nums) {
+            start = Math.max(start, j); // in the end of the loop this will contain the max item of the array
+            end += j;
+        }
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return end; // here start == end
+    }
 }
